76b06937d820a2414f5da8e57bce68cb3d1e44e3
Daan Wissing <daan.wissing@gmail.com>
2022-01-27 12:19:42 +0100

    Silen installers

--- Setup.ps1
+++ Setup.ps1
@@ -1,3 +1,6 @@
+# Props
+$baseRepo = 'https://raw.githubusercontent.com/daanwissing/setup/main'
+
 # Install winget
 
 $hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
@@ -15,18 +18,18 @@ if (!$hasPackageManager) {
 }
 
 Write-Host "Installing Powershell 7" -ForegroundColor Yellow
-winget install Microsoft.PowerShell
+winget install Microsoft.PowerShell --silent
 Write-Host 'Powershell 7 installed' -ForegroundColor Green
 
 
 Write-Host "Installing VS Code" -ForegroundColor Yellow
 
 # install VS Code
-winget install vscode
+winget install vscode --silent
 
 #extensions
 Write-Host "Installing VS Code extensions"
-$installBase = ${env:ProgramFiles}
+$installBase = "${env:USERPROFILE}/AppData/Local/Programs"
 $codeExePath = "$installBase\Microsoft VS Code\bin\code.cmd"
 $extensions = @("ms-vscode.Powershell", "ms-dotnettools.csharp")
 foreach ($extension in $extensions) {
@@ -38,23 +41,29 @@ Write-Host 'VS Code installed' -ForegroundColor Green
 
 # install Git
 Write-Host 'Installing git' -ForegroundColor Yellow
-winget install Git.Git 
+winget install Git.Git --silent
 
 #copying settings
 $dest = "$env:USERPROFILE/.gitconfig"
-$gitconfig_url = "https://raw.githubusercontent.com/daanwissing/setup/main/settings/.gitconfig"
+$gitconfig_url = "$baseRepo/settings/.gitconfig"
 Invoke-WebRequest -Uri $gitconfig_url -OutFile $dest
 Write-Host 'git installed' -ForegroundColor Green
 
 # install Keepass
-winget install KeePass 
+winget install KeePass --silent
 
 # install Terminal
 Write-Host "Installing Terminal" -ForegroundColor Yellow
-winget install Microsoft.WindowsTerminal
+winget install Microsoft.WindowsTerminal --silent
 
 # copying settings
 $dest = "$env:LocalAppData/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
-$terminalconfig_url = "https://raw.githubusercontent.com/daanwissing/setup/main/settings/terminal.json"
+$terminalconfig_url = "$baseRepo/settings/terminal.json"
 Invoke-WebRequest -Uri $terminalconfig_url -OutFile $dest
 Write-Host 'Terminal installed' -ForegroundColor Green
+
+# install Firefox
+$firefox_url = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+$dest = [System.IO.Path]::GetTempFileName() + ".exe"
+Invoke-WebRequest -Uri $firefox_url -OutFile $dest
+& $dest /S
